b6a151655b5e6eb5c76bc0e7d08756d836c3dea7
Alex <reyesdelsuenio@gmail.com>
2017-04-02 14:56:39 +0200

    Spinner

--- app/src/main/java/adaptadores/TiempoAdaptador.java
+++ app/src/main/java/adaptadores/TiempoAdaptador.java
@@ -1,8 +1,24 @@
 package adaptadores;
 
+import android.content.Context;
+import android.support.annotation.IdRes;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+import modelo.Tiempo;
+
 /**
  * Created by pamp on 02/04/17.
  */
 
-public class TiempoAdaptador {
+public class TiempoAdaptador extends ArrayAdapter<String> {
+
+
+    public TiempoAdaptador(@NonNull Context context, @LayoutRes int resource, @IdRes int textViewResourceId, @NonNull List<String> objects) {
+        super(context, resource, textViewResourceId, objects);
+    }
 }
--- app/src/main/java/es/pamp/eltiempo/TiempoFragmento.java
+++ app/src/main/java/es/pamp/eltiempo/TiempoFragmento.java
@@ -6,8 +6,11 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import modelo.Tiempo;
 
@@ -15,19 +18,27 @@ import modelo.Tiempo;
  * Created by pamp on 02/04/17.
  */
 
-public class TiempoFragmento extends Fragment {
-    Tiempo tiempo;
-    TextView ciudadTV;
-
-
+public class TiempoFragmento extends Fragment implements AdapterView.OnItemSelectedListener {
+    private Tiempo tiempo;
+    private TextView ciudadTV;
+    private Spinner ciudades;
+    private String ciudad;
+    private View v;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
-        View v = inflater.inflate(R.layout.tiempolayout,container,false);
-        tiempo = new Tiempo("Madrid");//TODO Borrar, ejemplo de inicialización
+        v = inflater.inflate(R.layout.tiempolayout,container,false);
+
+        // Inicialización de Spinner ciudades
+        ciudades = (Spinner)v.findViewById(R.id.ciudades_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(container.getContext(),R.array.ciudades_array, R.layout.support_simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ciudades.setAdapter(adapter);
+
         ciudadTV = (TextView)v.findViewById(R.id.ciudadTV);
+        ciudades.setOnItemSelectedListener(this);
 
         if (tiempo!=null){
             escribreTiempo(tiempo);
@@ -36,6 +47,9 @@ public class TiempoFragmento extends Fragment {
         return v;
 
     }
+    public void pideTiempo(String ciudad){
+        //TODO Hacer petición  http://api.openweathermap.org/data/2.5/weather?q=Madrid&appid=db87faa1ae2eb9f5b56d2d2bd6e11ff2&units=metric&lang=es
+    }
     public void escribreTiempo (Tiempo tiempo){
         ciudadTV.setText(tiempo.getCiudad());
     }
@@ -43,4 +57,18 @@ public class TiempoFragmento extends Fragment {
         this.tiempo = tiempo;
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if (position!=0) { //Comprueba que no se haya seleccionado el mensaje de: Seleccione localidad
+            ciudad = (String) parent.getItemAtPosition(position);
+            tiempo = new Tiempo(ciudad);
+            pideTiempo(ciudad);
+            escribreTiempo(tiempo);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
